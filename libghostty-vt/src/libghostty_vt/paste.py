91c2ae53b0aaf60db16f6579af423f0cf27573ff
trag1c <dev@jakubr.me>
2026-05-14 23:30:04 +0200

    reintroduce the workspace lol

--- libghostty-vt/src/libghostty_vt/paste.py
+++ libghostty-vt/src/libghostty_vt/paste.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-from libghostty._cffi import ffi, lib
-from libghostty.vt.errors import check_result
+from libghostty_vt._cffi import ffi, lib
+from libghostty_vt.errors import check_result
 
 
 def is_safe(data: bytes | str) -> bool:
